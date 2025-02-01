e93bd2c0d471aeac5c36dae5503738b94cc6f925
MrFishy <yu.tianle723@gmail.com>
2025-02-02 00:28:03 +0800

    Added class files and cleaned up comments

--- gitlet/Blob.java
+++ gitlet/Blob.java
@@ -9,20 +9,16 @@ import static gitlet.Utils.*;
 
 public class Blob implements Serializable {
 
-    public final byte[] contents;
-    public final String content_compare; //this will be the hashed value of contents in blob, and used as a name of blob.
-    public final String name;
+    public final byte[] contents; // serialised contents of file
+    public final String content_compare; // hashed value of contents in blob, and used as a name of blob.
+    public final String name; // name of file
 
 
-    //only handles files not directories. Need to check if only files are included and not directories by the question.
     public Blob(File file, String name){
         this.contents = readContents(file);
         this.content_compare = sha1(contents);
         this.name = name;
         }
-        //writeobject readobject
-
-    //a fruit for thought: if 2 files have the same content but different names, are they then considered the same file.
 
 
     }
